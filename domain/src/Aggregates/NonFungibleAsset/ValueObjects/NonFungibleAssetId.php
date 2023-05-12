fd9773ef8d8f7878697b667a766778c6d3541e76
osteel <yannickchenot@gmail.com>
2023-05-12 15:32:01 +0100

    removed redundant final keyword

--- domain/src/Aggregates/NonFungibleAsset/ValueObjects/NonFungibleAssetId.php
+++ domain/src/Aggregates/NonFungibleAsset/ValueObjects/NonFungibleAssetId.php
@@ -9,7 +9,7 @@ final readonly class NonFungibleAssetId implements AggregateRootId
 {
     private const NAMESPACE = '64d4faf2-d67a-4716-9b3c-bed1ca053068';
 
-    final private function __construct(public string $id)
+    private function __construct(public string $id)
     {
     }
 
